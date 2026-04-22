1dfaf9ad0f5bdb2d7b3ec47fda4c18e453891897
Alv3s04 <lukas.alves.christensen@gmail.com>
2026-04-23 00:32:59 +0200

    Controller and view for respectively User and Profile creating/updating. Updated module-info.java and pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,10 @@
-module dk.easv.examprojectweblager {
+module scanhub {
     requires javafx.controls;
     requires javafx.fxml;
     requires de.mkammerer.argon2.nolibs;
     requires com.microsoft.sqlserver.jdbc;
     requires java.sql;
     requires java.naming;
-
 
     exports ScanHub.GUI.controllers;
     opens ScanHub.GUI.controllers to javafx.fxml;
